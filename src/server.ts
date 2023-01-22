ae54aab2208465039d100d3ef2f4885217c8cb50
Henrique Fuschini <henriquefuschini@kissolutions.tech>
2023-01-22 15:55:43 -0300

    feat(back): create get habits route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-    return 'Hello World'
+app.get('/habit', async () => {
+    const habits = await prisma.habit.findMany()
+
+    return habits
 })
 
 app.listen({
     port: 3333
+}).then(() => {
+    console.log('Server running on port 3333');  
 })
