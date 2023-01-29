505bd6c8396fbb024abe8bedd8fbffa2ef9bfbe4
Henrique Fuschini <henriquefuschini@kissolutions.tech>
2023-01-29 12:59:48 -0300

    feat(back): add create habit route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,6 @@
+import dayjs from "dayjs"
 import { FastifyInstance } from "fastify"
+import { z } from 'zod'
 import { prisma } from "./lib/prisma"
 
 
@@ -9,4 +11,34 @@ export async function appRoutes(app: FastifyInstance) {
 
         return habits
     })
+
+    app.post('/habits', async (request) => {
+
+        const createHabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(
+                z.number().min(0).max(6)
+            )
+        })
+
+        const { title, weekDays } = createHabitBody.parse(request.body)
+
+        const today = dayjs().startOf('day').toDate()
+
+        await prisma.habit.create({
+            data: {
+                title,
+                created_at: today,
+                weekDays: {
+                    create: weekDays.map(weekDay => {
+                        return {
+                            week_day: weekDay
+                        }
+                    })
+                }
+            }
+        })
+
+    })
+
 }
