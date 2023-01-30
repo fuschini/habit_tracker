24cfdf5cee6a2f4612677f592b279be53d44c7e0
Henrique Fuschini <henriquefuschini@kissolutions.tech>
2023-01-30 10:18:26 -0300

    feat(mobile): display HabitDays placeholders with vertical scroll

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,11 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning.ts'
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
 const weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
-
 const datesFromYearBeginning = generateDatesFromYearBeginning()
+const minimumSummaryDatesSizes = 18 * 5
+const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearBeginning.length
 
 export function Home() {
     return (
@@ -24,13 +25,30 @@ export function Home() {
                     ))
                 }
             </View>
-            <View className="flex-row flex-wrap">
-                {
-                    datesFromYearBeginning.map(date => (
-                        <HabitDay key={date.toISOString()} />
-                    ))
-                }
-            </View>
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
+            >
+                <View className="flex-row flex-wrap">
+                    {
+                        datesFromYearBeginning.map(date => (
+                            <HabitDay key={date.toISOString()} />
+                        ))
+                    }
+
+                    {
+                        amountOfDaysToFill > 0 && Array
+                            .from({ length: amountOfDaysToFill })
+                            .map((_, i) => (
+                                <View
+                                    key={i}
+                                    className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                                    style={{ width: DAY_SIZE, height: DAY_SIZE }}
+                                />
+                            ))
+                    }
+                </View>
+            </ScrollView>
 
         </View>
     )
