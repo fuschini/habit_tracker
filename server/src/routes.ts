3772228497b7b362044b68f3405471a7cdadbc87
Henrique Fuschini <henriquefuschini@kissolutions.tech>
2023-01-30 18:20:32 -0300

    feat(back): create habit toggle route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -77,5 +77,53 @@ export async function appRoutes(app: FastifyInstance) {
         }
     })
 
+    app.patch('/habits/:habit_id/toggle', async (request) => {
+        const toggleHabitParams = z.object({
+            habit_id: z.string().uuid()
+        })
 
+        const { habit_id } = toggleHabitParams.parse(request.params)
+
+        // Endpoint only allows toggles habit for today
+        const today = dayjs().startOf('day').toDate()
+
+        let day = await prisma.day.findUnique({
+            where: {
+                date: today
+            }
+        })
+
+        if (!day) {
+            day = await prisma.day.create({
+                data: {
+                    date: today
+                }
+            })
+        }
+
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where: {
+                day_id_habit_id: {
+                    day_id: day.id,
+                    habit_id
+                }
+            }
+        })
+
+        if (dayHabit) {
+            await prisma.dayHabit.delete({
+                where: {
+                    id: dayHabit.id
+                }
+            })
+        } else {
+            await prisma.dayHabit.create({
+                data: {
+                    day_id: day.id,
+                    habit_id
+                }
+            })
+        }
+
+    })
 }
