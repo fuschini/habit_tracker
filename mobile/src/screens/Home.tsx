d628a44a6355c51fc278fd123d03b2a0c83339e7
Henrique Fuschini <henriquefuschini@kissolutions.tech>
2023-01-30 10:08:03 -0300

    feat(mobile): display HabitDays in Home

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,37 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning.ts'
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+
+const weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+
+const datesFromYearBeginning = generateDatesFromYearBeginning()
 
 export function Home() {
     return (
         <View className="flex-1 bg-background px-8 pt-16">
-            <Header></Header>
+            <Header />
+            <View className="flex-row mt-6 mb-2">
+                {
+                    weekDays.map((weekDays, i) => (
+                        <Text
+                            key={i}
+                            className="text-zinc-400 text-xl font-bold text-center mx-1"
+                            style={{ width: DAY_SIZE, height: DAY_SIZE }}
+                        >
+                            {weekDays}
+                        </Text>
+                    ))
+                }
+            </View>
+            <View className="flex-row flex-wrap">
+                {
+                    datesFromYearBeginning.map(date => (
+                        <HabitDay key={date.toISOString()} />
+                    ))
+                }
+            </View>
+
         </View>
     )
 }
